eae0a506511157c9b31d2e12359b26d5d6b58788
Abbybrown <98557317+Abbythedeveloper@users.noreply.github.com>
2023-07-24 21:41:23 -0400

    yeeun

--- portfolio-io-part2/src/active_menu.js
+++ portfolio-io-part2/src/active_menu.js
@@ -1,10 +1,10 @@
 const sectionIds = [
-  '#home',
-  '#about',
-  '#skills',
-  '#work',
-  '#testimonial',
-  '#contact',
+  "#home",
+  "#about",
+  "#skills",
+  "#work",
+  "#testimonial",
+  "#contact",
 ];
 const sections = sectionIds.map((id) => document.querySelector(id));
 const navItems = sectionIds.map((id) =>
@@ -15,7 +15,7 @@ const visibleSections = sectionIds.map(() => false);
 let activeNavItem = navItems[0];
 
 const options = {
-  rootMargin: '-20% 0px 0px 0px',
+  rootMargin: "-20% 0px 0px 0px",
   threshold: [0, 0.98],
 };
 const observer = new IntersectionObserver(observerCallback, options);
@@ -46,7 +46,7 @@ function findFirstIntersecting(intersections) {
 function selectNavItem(index) {
   const navItem = navItems[index];
   if (!navItem) return;
-  activeNavItem.classList.remove('active');
+  activeNavItem.classList.remove("active");
   activeNavItem = navItem;
-  activeNavItem.classList.add('active');
+  activeNavItem.classList.add("active");
 }
